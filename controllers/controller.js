d47d306fbdc5139667369ca888dd06e6a772b06e
Veronica <Ron@Veronicas-MacBook-Air.local>
2019-02-28 14:05:19 +0000

    day two

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,12 +1,16 @@
-const {analyseImage} = require('../model/model.js');
+const { analyseImage, findRecipes } = require("../model/model.js");
+const axios = require("axios");
 
 exports.getImageInfo = (req, res) => {
-    console.log(req.body);
+  //console.log(req.body);
 
-    analyseImage(req.body.url, (err, imageInfo) =>{
-        if (err)console.log(err)
-        else {
-        res.send(imageInfo)
-        }
-    });
-}
+  analyseImage(req.body.url).then(imageInfo => {
+    // console.log(imageInfo.images.classifiers.classes.class);
+    // console.log(typeof imageInfo);
+    let result = JSON.parse(imageInfo);
+    console.log(result.images);
+    //findRecipes(imageInfo);
+    //sync
+    //return promise
+  });
+};
--- model/model.js
+++ model/model.js
@@ -1,35 +1,34 @@
 const VisualRecognitionV3 = require("watson-developer-cloud/visual-recognition/v3");
 // const fs = require("fs");
-const apiKey = require('../config');
+const apiKey = require("../config");
 
 const visualRecognition = new VisualRecognitionV3({
   version: "2018-03-19",
-    iam_apikey: apiKey
+  iam_apikey: apiKey
 });
 
-exports.analyseImage = (url, cb) => {
+exports.analyseImage = url => {
+  return new Promise((resolve, reject) => {
+    const images_file = url;
+    const owners = ["IBM"];
+    const threshold = 0.6;
 
- // return new Promise((resolve, reject) => {
+    const params = {
+      url: images_file,
+      owners: owners,
+      threshold: threshold
+    };
 
-const images_file = url;
-const owners = ["IBM"];
-const threshold = 0.6;
-
-const params = {
-  url: images_file,
-  owners: owners,
-  threshold: threshold
+    visualRecognition.classify(params, function(err, response) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.stringify(response, null, 2));
+      }
+    });
+  });
 };
 
-visualRecognition.classify(params, function(err, response) {
-  if (err) { 
-    cb(err);
-  } else {
-    cb(null, response);
-   // resolve(JSON.stringify(response, null, 2))
-  }
-});
-
-//});
-
-}; 
+// exports.findRecipes = (foodItem, cb) => {
+//   return new Promise((resolve, reject) => {});
+// };

ccc3ba6ec530bd310e8f3eb6f86494dbd34f34f8
Andrew <andrewjworkman@googlemail.com>
2019-02-28 16:55:35 +0000

    Done EJS

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const apiRouter = require("./routers/apiRouter.js");
 
 app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+
+// app.get('/api', (req, res)=> {
+//     res.status(200).render('views')
+// });
+
 app.use("/api", apiRouter);
 
 module.exports = app;
--- controllers/controller.js
+++ controllers/controller.js
@@ -5,11 +5,27 @@ exports.getImageInfo = (req, res) => {
   //console.log(req.body);
 
   analyseImage(req.body.url).then(imageInfo => {
-    // console.log(imageInfo.images.classifiers.classes.class);
+    // console.log(imageInfo);
     // console.log(typeof imageInfo);
     let result = JSON.parse(imageInfo);
-    console.log(result.images);
-    //findRecipes(imageInfo);
+    let foodItem = result.images[0].classifiers[0].classes[0].class;
+    console.log(foodItem)
+    //console.log(imageInfo["classfiers"]);
+    
+    axios.get(`https://www.food2fork.com/api/search?key=58c867b3492af887ca4aaf0c7f19edea&q=${foodItem}&page=1`)
+  .then(function (response) {
+    //console.log(response.data);
+    let recipes = response.data;
+   // console.log(recipes);
+    res.render('views', recipes)
+    // res.send(response.data);
+
+    // <%= console.log(recipes[0].source_url) %>
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
     //sync
     //return promise
   });
